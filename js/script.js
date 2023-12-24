2175cf54385ad92f9b8928b6e15f5179e112344c
MiguelMolina16 <152354996+MiguelMolina16@users.noreply.github.com>
2023-12-24 08:14:11 -0500

    :wrench:  Fixes the greeting
    Fixes #4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener(
     "DOMContentLoaded",
     function(event) {
-        alert('The page is loaded and the script ran!')
+        var message = document.getElementById('mesagge')
+        message.innerText = 'The scritp ran!'
     }
 
 );
